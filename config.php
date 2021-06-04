b7ac21a45b8238a47acd0a3300254cbbd128694b
R3X-tr <71097904+R3X-tr@users.noreply.github.com>
2021-06-04 11:54:07 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$server = "localhost"; #if its n 
+$db_user = "";  #db user
+$db_pass = "";  #db password
+$database = "";   #database name(not name of the table)
+
+try {
+    $bgl = new PDO("mysql:host=$server;dbname=$database;charset=utf8", "$db_user", "$db_pass", [
+
+     ]);
+} catch ( PDOException $e ){
+	echo "hata";
+     print $e->getMessage();
+}
+?>
