34c74f05797b693d2805c7b37eafd91a342c02e4
Faruk Celik <71097904+R3X-tr@users.noreply.github.com>
2021-09-22 11:16:58 +0300

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include('config.php');
 
 $stmt = $bgl->prepare("SELECT * FROM x"); #x should be name of the table
 $stmt->execute(); 
-$data = $stmt->fetch();
+$data = $stmt->fetch(PDO::FETCH_ASSOC);
 echo($data);
 
 ?>
